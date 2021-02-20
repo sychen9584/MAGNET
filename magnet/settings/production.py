5956cb6292336f42094777c39c3677b50aab62c2
Shang-Yang Chen <sychen0584@gmail.com>
2021-02-19 18:33:56 -0600

    update settings

--- magnet/settings/production.py
+++ magnet/settings/production.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -8,3 +9,11 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+ALLOWED_HOSTS = ["magnet.herokuapp.com"]
+
+CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'America/Chicago'
